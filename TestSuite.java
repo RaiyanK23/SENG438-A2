71183410aeee7fe7b5085375da57c5c2cab3ef3d
RaiyanK23 <raiyankbr@gmail.com>
2023-02-09 20:50:50 -0800

    Adding to testsuite

--- TestSuite.java
+++ TestSuite.java
@@ -3,6 +3,9 @@
     getLowerBoundTest.class,
     getUpperBoundTest.class,
     getLength.class
+    ColumnTotalTest.class
+    NumberArrayTest.class
+    NumberArray2DTest.class
 })
   
 public class TestSuite { }
